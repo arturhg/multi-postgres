f08a10a19172f1d3ea4abe231db023414e075fda
Artur Arutyunyan <30243408+arturhg@users.noreply.github.com>
2019-10-01 21:01:06 +0400

    Remove superuser option, add extension installation option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,22 +15,6 @@ function create_databases_and_users {
 EOSQL
 }
 
-function create_databases_and_superusers {
-    local database=$1
-    local user=$2
-    local password=$3
-    echo "  Creating superuser '$user' with password '$password' and database '$database'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-        CREATE USER $user WITH PASSWORD '$password';
-        ALTER USER $user WITH SUPERUSER;
-        CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
-
-
-EOSQL
-}
-
 function create_database {
     local database=$1
     local user=$2
@@ -52,13 +36,13 @@ function create_user {
 EOSQL
 }
 
-function create_superuser {
-    local user=$1
-    local password=$2
-    echo "  Creating superuser '$user' with password '$password'"
+function install_extensions {
+    local database=$1
+    local extension=$2
+    echo "  Installing extension '$extension' on database '$database'"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-        CREATE USER $user WITH PASSWORD '$password';
-        ALTER USER $user WITH SUPERUSER;
+        \c $database
+        CREATE EXTENSION $extension;
 EOSQL
 }
 
@@ -68,25 +52,23 @@ if [ -n "$PG_DATABASES" ]; then
         read -r -a array_users <<< "$PG_USERS"
         read -r -a array_passwords <<< "$PG_PASSWORDS"
         if [ "${#array_databases[@]}" -eq "${#array_users[@]}" ] && [ "${#array_databases[@]}" -eq "${#array_passwords[@]}" ]; then
-            if [ -n "$PG_SUPERUSER" ] && [ "$PG_SUPERUSER" -eq "Y" ]; then
-                for key in "${!array_databases[@]}"; do
-                    create_databases_and_superusers "${array_databases[$key]}" "${array_users[$key]}" "${array_passwords[$key]}"
-                done
-            else
-                for key in "${!array_databases[@]}"; do
-                    create_databases_and_users "${array_databases[$key]}" "${array_users[$key]}" "${array_passwords[$key]}"
-                done
-            fi
+            for key in "${!array_databases[@]}"; do
+                create_databases_and_users "${array_databases[$key]}" "${array_users[$key]}" "${array_passwords[$key]}"
+            done
         elif [ "${#array_users[@]}" -eq 1 ] && [ "${#array_passwords[@]}" -eq 1 ]; then
-            if [ -n "$PG_SUPERUSER" ] && [ "$PG_SUPERUSER" -eq "Y" ]; then
-                create_superuser "${array_users[0]}" "${array_passwords[0]}"
-            else
-                create_user "${array_users[0]}" "${array_passwords[0]}"
-            fi
+            create_user "${array_users[0]}" "${array_passwords[0]}"
             for key in "${!array_databases[@]}"; do
                 create_database "${array_databases[$key]}" "${array_users[0]}" "${array_passwords[0]}"
             done
         fi
+    fi
+    if [ -n "$PG_EXTENSIONS" ]; then
+        read -r -a array_extensions <<< "$PG_EXTENSIONS"
+        for key in "${!array_databases[@]}"; do
+            for key2 in "${!array_extensions[@]}"; do
+                install_extensions "${array_databases[$key]}" "${array_extensions[$key2]}"
+            done
+        done
     fi
     echo "All databases succesfully created."
 fi
